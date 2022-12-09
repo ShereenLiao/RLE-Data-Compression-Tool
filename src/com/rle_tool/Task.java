e8811b50eade2eabd66920edd8b42f9944a7803e
ShereenLiao <xl3781@nyu.edu>
2022-12-08 20:22:25 -0500

    add Task

--- src/com/rle_tool/Task.java
+++ src/com/rle_tool/Task.java
@@ -13,6 +13,23 @@ public class Task implements Runnable{
     }
     @Override
     public void run() {
+        rle();
+    }
 
+    private StringBuilder rle(){
+        int count = 0;
+        char currentChar = Character.MIN_VALUE;//empty char
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < size; ++i){
+            if(i == 0 || chunk.get(i) == chunk.get(i - 1)){
+                ++count;
+            }
+            else {
+                builder.append(count).append(String.valueOf((char)currentChar));
+                count = 1;
+            }
+            currentChar = (char)chunk.get(i);
+        }
+        return builder;
     }
 }
